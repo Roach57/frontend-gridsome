a02cae3bc4baa459c6f2ca68881369efa7663a22
吴强 <wuqiang@datahunter.cn>
2021-05-09 22:57:22 +0800

    feat: 增加页头的音符效果

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,50 +1,160 @@
 <template>
   <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header>
-    <slot/>
+
+    <!-- Page Header -->
+    <section class="page-header" :style="'background-image: linear-gradient(120deg, '+backgroundColorLeft+', '+backgroundColorRight+');color: '+fontColor+';'">
+      <div v-for="(item,index) in randomIcon" :key="'ri'+index" :style="'position:absolute; top:'+item.top+'px; left:'+item.left+'px; z-index:1;'">
+        <font :style="'font-size: '+item.size+'px;color:#fff;'">
+          <b>♪</b>
+        </font>
+      </div>
+      <h1 class="project-name">{{blogTitle}}</h1>
+      <h2 class="project-tagline">{{blogDescribe}}</h2>
+      <a :href="'https://github.com/'+githubUsername" class="btn" target="_blank">GitHub主页</a>
+      <a href="https://github.com/Roach57/frontend-gridsome" class="btn" target="_blank">博客源码</a>
+    </section>
+
+    <!-- Main Content -->
+    <section class="main-content">
+      <el-row>
+        <el-col :span="6" style="padding-right:10px">
+          <sidebar></sidebar>
+        </el-col>
+        <el-col :span="18" style="padding-left:10px">
+          <slot/>
+        </el-col>
+      </el-row>
+    </section>
+
+    <!-- Foot -->
+    <section class="foot">
+      <foot></foot>
+    </section>
   </div>
 </template>
 
-<static-query>
-query {
-  metadata {
-    siteName
-  }
+<script>
+
+import Sidebar from '~/components/Sidebar'
+import Foot from '~/components/Foot'
+
+export default {
+  name: 'Layout',
+  components: {
+    Sidebar,
+    Foot
+  },
+  data() {
+    return {
+      blogTitle:'Roch57',
+      blogDescribe:'Welcome to my blog',
+      githubUsername: 'Roach57',
+      music: {
+        isPlay: false,
+        currentTime: 0,
+        maxTime: 0,
+        volume: 100
+      },
+      fullButton: {
+        full: false
+      },
+      topbar: {
+        active: "",
+      },
+      randomIcon: []
+    }
+  },
+  mounted() {
+    let width = window.innerWidth
+    for (let i = 0; i < 12; i++) {
+        let temp = {}
+        let left = parseInt(Math.random()*(width-310-10)+10);
+        if(left>width/2-150){
+            left+=300
+        }
+        temp["left"] = left
+        temp["top"] = parseInt(Math.random()*(300-20)+20);
+        temp["size"] = parseInt(Math.random()*(40-20)+20);
+        this.randomIcon.push(temp)
+    }
+},
 }
-</static-query>
+</script>
 
 <style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
+    .page-header {
+        padding: 5rem 6rem;
+        color: #fff;
+        text-align: center;
+        background-color: #159957;
+        background-image: linear-gradient(120deg, #155799, #159957);
+    }
 
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
+    .project-name {
+        font-size: 3.25rem;
+        margin-top: 0;
+        margin-bottom: 0.1rem;
+    }
 
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
+    .project-tagline {
+        font-size: 1.25rem;
+        margin-bottom: 2rem;
+        font-weight: normal;
+        opacity: 0.7;
+    }
 
-.nav__link {
-  margin-left: 20px;
-}
+    .btn:hover {
+        color: rgba(255, 255, 255, 0.8);
+        text-decoration: none;
+        background-color: rgba(255, 255, 255, 0.2);
+        border-color: rgba(255, 255, 255, 0.3);
+    }
+
+    a:hover {
+        text-decoration: underline;
+    }
+
+    a:active,
+    a:hover {
+        outline: 0;
+    }
+
+    .btn {
+        padding: 0.75rem 1rem;
+        display: inline-block;
+        margin-bottom: 1rem;
+        color: rgba(255, 255, 255, 0.7);
+        background-color: rgba(255, 255, 255, 0.08);
+        border-color: rgba(255, 255, 255, 0.2);
+        border-style: solid;
+        border-width: 1px;
+        border-radius: 0.3rem;
+        transition: color 0.2s, background-color 0.2s, border-color 0.2s;
+    }
+
+    a {
+        color: #1e6bb8;
+        text-decoration: none;
+    }
+
+    .btn+.btn {
+        margin-left: 1rem;
+    }
+
+    .main-content {
+        max-width: 64rem;
+        padding: 30px 0px 30px 0px;
+        margin: 0 auto;
+        font-size: 1.1rem;
+        word-wrap: break-word;
+        min-height: 800px;
+    }
+
+    .foot {
+        max-width: 67rem;
+        margin: 0 auto;
+        font-size: 12px !important;
+        color: #586069 !important;
+        word-wrap: break-word;
+    }
 </style>
